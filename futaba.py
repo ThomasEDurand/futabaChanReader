f049f38aae974f38084b6be158cf328029168c48
tedurand <tedurand@live.unc.edu>
2023-03-09 09:41:59 -0500

    "added function to render display to improved readability, improved archive feature, fixed board table giving incorrect board ids"

--- futaba.py
+++ futaba.py
@@ -15,29 +15,41 @@ import climage
 
 lastThreadURL = None
 header = Panel("ふたばちゃんは、生まれたばかりの掲示板です。")
+console = Console()
 
 
-def get_boards(boards):
+def getBoardsTable(boards):
     l = len(boards)
-    third = math.ceil(l / 3)
+    third = math.ceil(l/3)
     table = Table()
     table.add_column("1~" + str(third), no_wrap=True, min_width=30)
     table.add_column(str(third) + "~" + str(third * 2), no_wrap=True, min_width=30)
     table.add_column(str(2 * third) + "~" + str(l), no_wrap=True, min_width=30)
 
-    for i in range(0, third):
-        if i + third * 2 < l:
-            table.add_row(str(i + 1) + "\t" + boards[i][1], str(i + third) + "\t" + boards[i + third][1],
-                          str(i + third * 2) + "\t" + boards[i + third * 2][1])
-        elif i + third < l:
-            table.add_row(str(i + 1) + "\t" + boards[i][1], str(i + third) + "\t" + boards[i + third][1], "")
+    for i in range(1, third+1):
+        if i+third*2 <= l:
+            table.add_row(str(i) + "\t" + boards[i-1][1], str(i+third) + "\t" + boards[i-1+third][1],
+                          str(i+third*2) + "\t" + boards[i-1+third*2][1])
+        elif i+third < l:
+            table.add_row(str(i) + "\t" + boards[i-1][1], str(i+third) + "\t" + boards[i-1+third][1], "")
         else:
-            table.add_row(str(i + 1) + "\t" + boards[i][1], "", "")
+            table.add_row(str(i) + "\t" + boards[i-1][1], "", "")
 
     return table
 
 
-def board(boardID, console, boards):
+def getBoards():
+    url = urllib.request.urlopen('https://www.2chan.net/index2.html')
+    soup = BeautifulSoup(url, 'html.parser')
+    boards = []
+    for i, td in enumerate(soup.find_all('td')):
+        for j, a in enumerate(td.find_all('a')):
+            boards.append((a.get('href'), a.text))
+    return boards
+
+
+def board(boardID, boards):
+    global console
     console.clear()
     threadID = 1
     # Browsing Board
@@ -67,23 +79,21 @@ def board(boardID, console, boards):
                 threadTable.add_row(str(i + 1), threadTitle, replies)
                 threads.append(td.find('a').get('href'))
 
-        console.print(threadTable)
-        # Browsing Thread
-        threadID = int(Prompt.ask("(zero to break) thread no "))
+        threadID = int(renderDisp(threadTable, "(zero to break) thread no "))
         console.clear()
 
         consoleTable = Table(show_lines=True)
         consoleTable.add_column("Thread No", no_wrap=False, min_width=40)
         consoleTable.add_column("Text", no_wrap=False, min_width=60)
         consoleTable.add_column("Img", no_wrap=True, min_width=10)
-        viewThread(threads, threadID, b, console, consoleTable)
-        console.print(header)
-        console.print(threadTable)
-        threadID = int(Prompt.ask("(zero to break) thread no "))
+
+        viewThread(threads, threadID, b, consoleTable)
+        threadID = int(renderDisp(threadTable, "(zero to break) thread no "))
     console.clear()
 
 
-def viewThread(threads, threadID, b, console, consoleTable):
+def viewThread(threads, threadID, b, consoleTable):
+    global console
     cat = 'https:' + b[0:len(b) - 10] + threads[threadID - 1]
     url = urllib.request.urlopen(cat)
     global lastThreadURL
@@ -102,7 +112,9 @@ def viewThread(threads, threadID, b, console, consoleTable):
         blockquote = table.find('blockquote')
         if blockquote is not None:
             paragraph = ""
-            threadInfo = table.find('span', {"class": "csb"}).text + " " + table.find('span', {"class": "cnm"}).text + " " + table.find('span', {"class": "cnw"}).text + " " + table.find('span', {"class": "cno"}).text
+            threadInfo = table.find('span', {"class": "csb"}).text + " " + table.find('span', {
+                "class": "cnm"}).text + " " + table.find('span', {"class": "cnw"}).text + " " + table.find('span', {
+                "class": "cno"}).text
             for j, tag in enumerate(blockquote):
                 paragraph = paragraph + tag.text
 
@@ -117,26 +129,22 @@ def viewThread(threads, threadID, b, console, consoleTable):
                 # imageURL = "https://nov.2chan.net" + image['src'].replace("thumb", "src")[0:-5] + ".jpg"  # source
             consoleTable.add_row(threadInfo, paragraph, imageURL)
             if i % 10 == 9:
-                console.clear()
-                console.print(header)
-                console.print(consoleTable)
-                cont = Prompt.ask("Next ten: y/n ")
+                cont = renderDisp(consoleTable, "Next ten: y/n")
                 if cont == "n" or cont == "N":
                     breakThread = True
             if breakThread:
                 break
 
-    console.clear()
-    console.print(consoleTable)
-    if Prompt.ask("press a to archive thread") == "a":
-        archiveThread = threading.Thread(target=archive, args=(console,))
+    if renderDisp(consoleTable, "press a to archive thread") == "a":
+        archiveThread = threading.Thread(target=archive)
         archiveThread.start()
     console.clear()
 
 
-def archive(console):
+def archive():
     global lastThreadURL
     global header
+    global console
     if lastThreadURL is not None:
         header = Panel("archiving", style="blink")
         user_agent = "Mozilla/5.0 (Windows NT 5.1; rv:40.0) Gecko/20100101 Firefox/40.0"
@@ -147,30 +155,29 @@ def archive(console):
         f.write(archivedURL + "\n")
         f.close()
     else:
-        console.print("no threads visited")
+        header = Panel("no threads visited")
+
+
+def renderDisp(table, prompt):
+    global console
+    global header
+    console.clear()
+    console.print(header)
+    console.print(table)
+    if prompt is not None or prompt != "":
+        return Prompt.ask(prompt)
+    return -1
 
 
 def main():
 
-    console = Console()
-    # INITIAL SCAN THROUGH BOARDS CREATE A LIST OF TUPLES
-    url = urllib.request.urlopen('https://www.2chan.net/index2.html')
-    soup = BeautifulSoup(url, 'html.parser')
-
-    boards = []
-    for i, td in enumerate(soup.find_all('td')):
-        for j, a in enumerate(td.find_all('a')):
-            boards.append((a.get('href'), a.text))
-
-    boardsTable = get_boards(boards)
-    console.clear()
+    boards = getBoards()  # array of tuples
+    boardsTable = getBoardsTable(boards)  # table, object that only needs to be generated once
     while True:
-        console.print(header)
-        console.print(boardsTable)
-        bID = Prompt.ask("Go to board")
-        if bID == 0 or bID == "e" or bID == "exit" or bID == "q" or bID == "quit":
+        bID = renderDisp(boardsTable, "Go to board")
+        if bID == "0" or bID == "e" or bID == "exit" or bID == "q" or bID == "quit":
             return 0
-        board(int(bID) + 1, console, boards)
+        board(int(bID), boards)
 
 
 main()
